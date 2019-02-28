9109b25583baeeeb6ef31f8afe0fb144add66cc7
leiwei <leiwei0312@163.com>
2019-02-28 19:57:42 +0800

    添加工程到远程仓库

--- src/main/java/cn/itheima/Test.java
+++ src/main/java/cn/itheima/Test.java
@@ -11,6 +11,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaa");
         System.out.println("bbbbbbbbbb");
+        System.out.println("bbbbbbbbbb");System.out.println("aaaaaaaaaaaa");
+        System.out.println("bbbbbbbbbb");
         System.out.println("bbbbbbbbbb");
 
     }
