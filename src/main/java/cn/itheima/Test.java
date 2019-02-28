0997cd011f0339a63c5030c712a0ae6b9dc09f1d
雷聪 <leicong@foxmail.com>
2019-02-28 19:36:18 +0800

    添加工程到本地仓库

---
+++ src/main/java/cn/itheima/Test.java
@@ -0,0 +1,14 @@
+package cn.itheima;
+
+/**
+ * @Description:
+ * @Author: AllenLei (leicong@foxmail.com)
+ * @date: 2019/02/28
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaa");
+    }
+}
